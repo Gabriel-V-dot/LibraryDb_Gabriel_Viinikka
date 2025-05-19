3bc3c8926958cdad07598ab679409232c7c00ebf
Gabriel-V-Dot <gabriel.viinikka@gmail.com>
2025-05-19 12:15:35 +0200

    Refactored test1 to isbnValidatorTest with all scenarios separate, added working function for validation of modern ISBN numbers.

--- LibraryDb_Gabriel_Viinikka/Services/IsbnValidator.cs
+++ LibraryDb_Gabriel_Viinikka/Services/IsbnValidator.cs
@@ -1,4 +1,7 @@
-﻿namespace LibraryDb_Gabriel_Viinikka.Services
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace LibraryDb_Gabriel_Viinikka.Services
 {
     public class IsbnValidator
     {
@@ -8,13 +11,82 @@
             
         }
 
-        public bool TenValidator()
+        public bool Validate(string isbn)
         {
+            bool valid = false;
+
+            if (IsbnLenghtValid(isbn))
+            {
+                if(isbn.Length == 13)
+                {
+                    valid = ThirteenValidator(isbn);
+                }
+                else
+                {
+                    valid = TenValidator(isbn);
+                }
+            }
+
+            return valid;
+        }
+
+        private bool IsbnLenghtValid(string isbn)
+        {
+            bool valid = false;
+
+            Debug.WriteLine($"{isbn} Length: {isbn.Length}");
+
+            if (isbn == null)
+            {
+                return valid;
+            }
+            else if (isbn == string.Empty)
+            {
+                return valid;
+            }
+            else if (isbn.Length != 10 && isbn.Length != 13)
+            {
+                return valid;
+            }
+
             return true;
         }
-        public bool ThirteenValidator()
+
+        private bool TenValidator(string isbn)
         {
-            return false;
+            bool valid = false;
+
+
+
+            return valid;
+        }
+        private bool ThirteenValidator(string isbn)
+        {
+            bool valid = false;
+
+
+            int checkSum = 0;
+            int counter = 1;
+            foreach (char number in isbn)
+            {
+                bool parsed = int.TryParse(number.ToString(), out int parsedNumber);
+
+                bool even = counter % 2 == 0 ? true : false;
+
+                if (even) checkSum += parsedNumber * 3;
+                if (!even) checkSum += parsedNumber * 1;
+
+                counter++;
+            }
+
+            Math.DivRem(checkSum, 10,out int remainder);
+
+            if(remainder == 0)
+            {
+                valid = true;
+            }
+
+            return valid;
         }
 
     }
--- ValidatorTest.Tests/Test1.cs
+++ ValidatorTest.Tests/Test1.cs
@@ -3,14 +3,81 @@
 namespace ValidatorTest.Tests
 {
     [TestClass]
-    public sealed class Test1
+    public sealed class IsbnValidatorTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IsbnInvalidTest()
         {
-            //assert
-            var isbnValidator = new IsbnValidator();
+            #region Arrange
+
+            var validator = new IsbnValidator();
+            string notIsbn = "123";
+
+            #endregion
+
+            #region Act
+            bool oneTwoThree = validator.Validate(notIsbn);
+            #endregion
+
+            #region Assert
+            Assert.IsFalse(oneTwoThree);
+            #endregion
 
         }
+
+        [TestMethod]
+        public void IsbnEmptyTest()
+        {
+            #region Arrange
+            var validator = new IsbnValidator();
+
+            string empty = string.Empty;
+            #endregion
+
+            #region Act
+            bool isEmpty = validator.Validate(empty);
+            #endregion
+
+            #region Assert
+            Assert.IsFalse(isEmpty);
+            #endregion
+        }
+
+        [TestMethod]
+        public void IsbnThirteenTest()
+        {
+            #region Arrange
+            var validator = new IsbnValidator();
+            string thirteenNumeral = "9781599325743";
+            #endregion
+
+            #region Act
+            bool fuMoney = validator.Validate(thirteenNumeral);
+            #endregion
+
+            #region Assert
+            Assert.IsTrue(fuMoney);
+            #endregion
+        }
+        [TestMethod]
+        public void IsbnTenTest()
+        {
+            #region Arrange
+
+            var validator = new IsbnValidator();
+            string tenNumeral = "9120059175";
+
+
+            #endregion
+
+            #region Act
+            bool returnOfTheKing = validator.Validate(tenNumeral);
+            #endregion
+
+            #region Assert
+            Assert.IsTrue(returnOfTheKing);
+            #endregion
+        }
+
     }
 }
