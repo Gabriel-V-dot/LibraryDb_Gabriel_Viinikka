1759097c9f996183e06c392cc572a408b221fdad
Gabriel-V-Dot <gabriel.viinikka@gmail.com>
2025-08-25 11:37:42 +0200

    Refined datehandling and fixed date bug in ActiveLoanDTOExtensionMethod.

--- LibraryDb_Gabriel_Viinikka/DTOs/DTOExtensions/LoansDTOExtensions.cs
+++ LibraryDb_Gabriel_Viinikka/DTOs/DTOExtensions/LoansDTOExtensions.cs
@@ -12,8 +12,8 @@ namespace LibraryDb_Gabriel_Viinikka.DTOs.DTOExtensions
             return new ActiveLoanDTO
             {
                 Id = loan.Id,
-                LoanDate = new DateOnly (loan.LoanDate.Year,loan.LoanDate.Month,loan.LoanDate.Day),
-                ExpectedReturnDate = new DateOnly(loan.LoanDate.Year, loan.LoanDate.Month, loan.LoanDate.Day + loan.Inventory.DaysToLoan),
+                LoanDate = DateOnly.FromDateTime(loan.LoanDate),  //new DateOnly (loan.LoanDate.Year,loan.LoanDate.Month,loan.LoanDate.Day),
+                ExpectedReturnDate = DateOnly.FromDateTime(loan.LoanDate.AddDays(loan.Inventory.DaysToLoan)), // new DateOnly(loan.LoanDate.Year, loan.LoanDate.Month, loan.LoanDate.Day + loan.Inventory.DaysToLoan),
                 Inventory = loan.Inventory.ToMinimalInventoryDTO(),
                 LoanCard = loan.LoanCard.ToMinimalLoanCardDTO(),
                 DaysToLoan = (int)(loan.LoanDate.AddDays(loan.Inventory.DaysToLoan) - loan.LoanDate).TotalDays  //(int)(loan.LoanDate - loan.LoanDate.AddDays(loan.Inventory.DaysToLoan)).TotalDays
@@ -24,7 +24,7 @@ namespace LibraryDb_Gabriel_Viinikka.DTOs.DTOExtensions
             return new ReturnedLoanDTO
             {
                 Id = loan.Id,
-                LoanDate = new DateOnly(loan.LoanDate.Year, loan.LoanDate.Month, loan.LoanDate.Day),
+                LoanDate = DateOnly.FromDateTime(loan.LoanDate),  //new DateOnly(loan.LoanDate.Year, loan.LoanDate.Month, loan.LoanDate.Day),
                 ReturnDate = DateOnly.FromDateTime((DateTime)loan.ReturnDate!),
                 Inventory = loan.Inventory.ToMinimalInventoryDTO(),
                 LoanCard = loan.LoanCard.ToMinimalLoanCardDTO(),
--- ValidatorTest.Tests/LoansDTOExtensionsTest.cs
+++ ValidatorTest.Tests/LoansDTOExtensionsTest.cs
@@ -4,6 +4,9 @@ using LibraryDb_Gabriel_Viinikka.DTOs.InventoryDTOs;
 using LibraryDb_Gabriel_Viinikka.DTOs.LoanCardDTOs;
 using LibraryDb_Gabriel_Viinikka.DTOs.LoansDTOs;
 using LibraryDb_Gabriel_Viinikka.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.CodeAnalysis.FlowAnalysis.DataFlow;
+using System.Diagnostics;
 
 namespace ValidatorTest.Test
 {
@@ -13,65 +16,69 @@ namespace ValidatorTest.Test
         [TestMethod]
         public void ToActiveLoanDTOTest()
         {
-            #region Arrange
-            #region SettingUpLoan
-            DateTime presentTime = DateTime.Now;
-            DateTime futureTime = DateTime.Now.AddDays(1);
+           
+                #region Arrange
+                #region SettingUpLoan
+                DateTime presentTime = DateTime.Now;
+                DateTime futureTime = DateTime.Now.AddDays(1);
 
 
-            Loan loan = new Loan
-            {
-                Id = 1,
-                LoanDate = presentTime,
-                ReturnDate = futureTime,
-                Inventory = new Inventory
+                Loan loan = new Loan
                 {
                     Id = 1,
-                    BookId = 1,
-                    Book = new Book
+                    LoanDate = presentTime,
+                    ReturnDate = futureTime,
+                    Inventory = new Inventory
                     {
                         Id = 1,
-                        Title = "Testing Book",
-                        ISBN = "9781471129391",
-                        PublicationDate = new DateOnly(1999, 01, 01),
-                        Authors = [new Author
+                        BookId = 1,
+                        Book = new Book
+                        {
+                            Id = 1,
+                            Title = "Testing Book",
+                            ISBN = "9781471129391",
+                            PublicationDate = new DateOnly(1999, 01, 01),
+                            Authors = [new Author
                         {
                             Id = 1,
                             FirstName = "Testing Author",
                             LastName = "Testing Author LastName"
                         }
-                        ]
+                            ]
+                        },
+                        DaysToLoan = 1
                     },
-                    DaysToLoan = 1
-                },
-                LoanCard = new LoanCard 
+                    LoanCard = new LoanCard
+                    {
+                        Id = 1,
+                        CreationDate = presentTime,
+                        ExpirationDate = futureTime
+                    }
+
+                };
+
+                ActiveLoanDTO actualActiveLoanDTO = loan.ToActiveLoanDTO();
+                ActiveLoanDTO expectedActiveLoanDTO = new ActiveLoanDTO
                 {
                     Id = 1,
-                    CreationDate = presentTime,
-                    ExpirationDate = futureTime
-                }
+                    LoanDate = DateOnly.FromDateTime(presentTime),
+                    ExpectedReturnDate = DateOnly.FromDateTime(futureTime),
+                    Inventory = loan.Inventory.ToMinimalInventoryDTO(),
+                    LoanCard = loan.LoanCard.ToMinimalLoanCardDTO(),
+                    DaysToLoan = 1
+                };
 
-            };
-
-            ActiveLoanDTO actualActiveLoanDTO = loan.ToActiveLoanDTO();
-            ActiveLoanDTO expectedActiveLoanDTO = new ActiveLoanDTO
-            {
-                Id = 1,
-                LoanDate = DateOnly.FromDateTime(presentTime),
-                ExpectedReturnDate = DateOnly.FromDateTime(futureTime),
-                Inventory = loan.Inventory.ToMinimalInventoryDTO(),
-                LoanCard = loan.LoanCard.ToMinimalLoanCardDTO(),
-                DaysToLoan = 1
-            };
-
-            #endregion
-            #endregion
-            #region Act
-            loan.ToActiveLoanDTO();
-    #endregion
-    #region Assert
-            Assert.AreEqual(actualActiveLoanDTO, expectedActiveLoanDTO);
-    #endregion
+                #endregion
+                #endregion
+                #region Act
+                loan.ToActiveLoanDTO();
+                #endregion
+                #region Assert
+                Assert.AreEqual(actualActiveLoanDTO.ExpectedReturnDate, expectedActiveLoanDTO.ExpectedReturnDate);
+                #endregion
+            
+       
+            
 
 }
     }
